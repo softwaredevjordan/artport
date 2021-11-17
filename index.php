a6ee704d8c993722c377884a6620c5eaa8227f6d
Jordan H <webdevjordan@protonmail.com>
2021-11-17 06:25:31 -0600

    Got modals working correctly now need to make them pretty

--- index.php
+++ index.php
@@ -13,15 +13,32 @@ get_header();
         while($mainLoop->have_posts()) {
             $mainLoop->the_post();
             ?>    
-    
-                
-                <div class="post">
+            
+                <div class="post" id=>
                     <a href="<?php the_permalink(); ?>">
+                    <button data-toggle="modal" onclick="javascript: return false;"  data-target="#<?php the_title() ?>">
                     <img class="images"  src="<?php the_post_thumbnail_url($size = 'medium') ?>" alt="">
+                    </button>
                     <p class="title"><?php the_title() ?></p>
                     </a>   
                 </div>
+            
 
+            <div class="modal" id="<?php the_title() ?>" role="dialog">
+                <div class="modal-dialog">
+                    <div class= "modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title"><?php the_title() ?></h4>
+                        </div>
+                        <div class="modal-body">
+                            <img src="<?php the_post_thumbnail_url($size = 'medium') ?>" alt="">
+                        </div>
+                        <div class="modal-footer"></div>
+                        <p>alskjf</p>
+                    </div>
+                </div>
+            </div>
          <?php
         }
         wp_reset_postdata();
--- js/index.js
+++ js/index.js
@@ -66,3 +66,5 @@ function ajaxButtonPass(button){
 }
 
 
+
+
